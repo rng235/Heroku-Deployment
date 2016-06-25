dbbe44a95df079ea3de309aa682cc959b2b3eda7
Ricardo Guntur <rng235@nyu.edu>
2016-06-25 16:52:54 -0400

    Added views. Added router to lionsden

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,61 @@ router.get('/', function (req, res) {
     res.render('home');
 });
 
+router.get('/lionsden', function (req, res) {
+    console.log("---------------------------Load Lion's Den Page---------------------------");
+    //Find Elizabeth's poems to display
+
+    //If anyone is logged on
+    if (!!req.user) {
+
+        //If admin is logged in
+        if (req.user.username == "ed1340") {
+
+            EPoem.find(function (err, Poem, count) {
+                console.log("---------Find Poem---------");
+                //console.log(err, Poem, count);
+
+                if (err) {
+                }
+
+                else {
+                    res.render('lionsdenadmin', {Poem: Poem});
+                }
+            });
+        }
+
+        //Else display Elizabeth's poetry
+        else {
+            EPoem.find(function (err, Poem, count) {
+                console.log("---------Find Poem---------");
+                //console.log(err, Poem, count);
+
+                if (err) {
+
+                }
+
+                else {
+                    res.render('lionsden', {Poem: Poem});
+                }
+            });
+        }
+    }
+
+    //Else display Elizabeth's poetry
+    else {
+        EPoem.find(function (err, Poem, count) {
+            console.log("---------Find Poem---------");
+            //console.log(err, Poem, count);
+
+            if (err) {
+
+            }
+
+            else {
+                res.render('lionsden', {Poem: Poem});
+            }
+        });
+    }
+});
 
 module.exports = router;
