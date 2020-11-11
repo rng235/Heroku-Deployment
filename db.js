9f0481a306bcc1d79ad1a4efd2dcfe727914f89e
Ricardo N. Guntur <rng235@nyu.edu>
2020-11-11 13:55:24 -0500

    Update db.js

--- db.js
+++ db.js
@@ -43,5 +43,5 @@ mongoose.model('userPoem', userPoem);
 //mongoose.connect('mongodb://localhost/final');
 
 //For heroku deployment
-mongoose.connect('mongodb://rng235:Firecold1@ds023054.mlab.com:23054/heroku_x91l02fz');
+mongoose.connect('mongodb://rng235:!Firecold6031241@ds023054.mlab.com:23054/heroku_x91l02fz');
 
